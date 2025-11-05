a37d60a95c0a9f810cf4f26b6030326a8b19a842
Likhon <likhon.929@gmail.com>
2025-11-05 22:44:22 +0600

    Refactor routes and add portfolio view

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Get route example
 Route::get('/', function () {
     return view('home');
 });
 
+// Contact route example
 Route::get('/contact', function () {
     return view('contact');
+});
+
+// Parameters using route
+Route::get('/portfolio/{firstname}/{lastname}', function ($firstname, $lastname) {
+    // return view('portfolio');
+    return "Welcome to my portfolio page. My name is " . $firstname . " " . $lastname . ".";
 });
