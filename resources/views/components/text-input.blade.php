22053834e3d2ac3a66dc4e374d8a357eb93e2bb6
Likhon <likhon.929@gmail.com>
2025-11-22 22:37:08 +0600

    Add text input component with optional disabled state

---
+++ resources/views/components/text-input.blade.php
@@ -0,0 +1,3 @@
+@props(['disabled' => false])
+
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-2 focus:ring-indigo-500/20 dark:focus:ring-indigo-600/20 rounded-lg shadow-sm w-full transition duration-150 ease-in-out']) !!}>
